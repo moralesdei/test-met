76685eb919df877a9fbdc2dc1ae3ae11b06f0e47
Andres Morales <moralesdei@protonmail.com>
2021-02-17 01:01:47 -0500

    I finish point1

--- handle_matrix.py
+++ handle_matrix.py
@@ -3,23 +3,50 @@
 # Prueba para la empresa MET GROUP 2020
 # Punto No 1
 # Deimer Andres Morales Herrera
-from numpy import asarray
-
 class MyMatrix():
     def __init__(self, matrix):
-        self.__matrix = matrix;
-        self.countDimension = 0;
+        self.__matrix = matrix
+        self.__countDimension = 0
+        self.__sumMatrix = 0
+        self.__flagStraight = True
+
     def dimension(self, multiList = None):
         if multiList is None:
           multiList = self.__matrix
         if type(multiList) is list:
-          self.countDimension += 1
-          self.dimension(multiList[0])       
-        else:
-          print(self.countDimension)
-          return None
-
+          self.__countDimension += 1      
+          self.dimension(multiList[0])
+        # when multiList is not a list anymore, we can return the matrix dimension.       
+        else:        
+          print(self.__countDimension)
+    
     def straight(self):
-        pass
+        self.__parsedMatrix()
+        print(self.__flagStraight)
 
-        
+    def compute(self):
+        self.__parsedMatrix()
+        print(self.__sumMatrix)   
+
+    def __parsedMatrix(self):
+            self.__sumMatrix = 0
+            if self.__countDimension == 1:
+                self.__sumMatrix = sum(self.__matrix)
+            elif self.__countDimension == 2:
+                checkNumberElements = len(self.__matrix[0])
+                for i in range(len(self.__matrix)):
+                  self.__sumMatrix += sum(self.__matrix[i])
+                  if(len(self.__matrix[i]) != checkNumberElements):
+                    self.__flagStraight = False     
+            # With this one, I check every dimension bigger than 2D.             
+            else:   
+                for i in range(len(self.__matrix)):
+                  checkNumberElements = len(self.__matrix[i][0])
+                  for j in range(len(self.__matrix[i])):
+                      self.__sumMatrix += sum(self.__matrix[i][j])
+                      if(len(self.__matrix[i][j]) != checkNumberElements):
+                        self.__flagStraight = False
+                
+
+    
+
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # encoding utf-8
-# Prueba para la empresa MET GROUP 2020
-# Punto No 1
+# Test for company MET GROUP 2020
+# Point No 1
 # Deimer Andres Morales Herrera
 import handle_matrix as hm
 
 if __name__ == '__main__':
+
     # Subjects to do tests.
     a = [1,2]
     b = [[1,2],[2,4]]
@@ -13,6 +14,10 @@ if __name__ == '__main__':
     d = [[[3,4],[6,5]]]
     e = [[[1,2,3]],[[5,6,7],[5,4,3]],[[3,5,6],[4,8,3],[2,3]]]
     f = [[[1,2,3],[2,3,4]],[[5,6,7],[5,4,3]],[[3,5,6],[4,8,3]]]
+
     # Here I'm making instances from MyMatrix's objects.
-    o = hm.MyMatrix(e)
+
+    o = hm.MyMatrix(c)
     o.dimension()
+    o.straight()
+    o.compute()
